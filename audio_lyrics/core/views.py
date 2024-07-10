44c8bf581d5dfcdb0b45136333f895a3664eea62
fasasimayokun <maykay.fash@gmail.com>
2024-07-10 17:10:44 +0100

    index template commit

--- audio_lyrics/core/views.py
+++ audio_lyrics/core/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    pass
+
+def user_login(request):
+    pass
+
+def user_registration(request):
+    pass
+
+def user_logout(request):
+    pass
