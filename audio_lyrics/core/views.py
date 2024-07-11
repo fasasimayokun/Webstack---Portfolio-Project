c84e87616e57851fbceb1102daeb7d58cc4bef24
fasasimayokun <maykay.fash@gmail.com>
2024-07-11 10:11:37 +0100

    login and registration logic

--- audio_lyrics/core/views.py
+++ audio_lyrics/core/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
@@ -6,10 +8,38 @@ def index(request):
     return render(request, 'index.html')
 
 def user_login(request):
+    
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            redirect('/')
     return render(request, 'user_auth/login.html')
 
 def user_registration(request):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        repeatPassword = request.POST['repeatPassword']
+
+        if password == repeatPassword:
+            try:
+                user = User.objects.create_user(username,email,password)
+                user.save()
+                login(request, user)
+                redirect('/')
+            except:
+                pass
+        else:
+            pass
+
     return render(request, 'user_auth/registration.html')
 
 def user_logout(request):
-    pass
+    logout(request)
